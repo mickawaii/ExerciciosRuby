82eff7f3864e4318c6a72cfc748693c4e1173267
mick <mi.lee>
2013-05-02 20:04:08 -0300

    Parte2a.rb incluida

--- parte2a.rb
+++ parte2a.rb
@@ -3,6 +3,12 @@ class NoSuchStrategyError < StandardError ; end
 
 def rps_game_winner(game)
 	raise WrongNumberOfPlayersError unless game.length == 2 
+	game[0][1].upcase!
+	game[1][1].upcase!
+	str = ['R', 'P', 'S']
+	if(!str.member?(game[0][1]) || !str.member?(game[1][1]))
+		raise NoSuchStrategyError
+	end
 	if(game[0][1] == game[1][1])
 		return game[0][0]
 	elsif((game[0][1] == 'R' && game[1][1] == 'S') || 
@@ -12,5 +18,4 @@ def rps_game_winner(game)
 	else
 		return game[1][0]
 	end
-	puts game.uppercase
 end
