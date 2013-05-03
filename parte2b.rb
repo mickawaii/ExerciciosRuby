1e29435c3b45ac42f0bf84c230a1735c63ad19d2
Daniel Sousa <daniel.sousa@codus.com.br>
2013-05-03 11:16:19 -0300

    Exercicio 2 terminado

--- parte2b.rb
+++ parte2b.rb
@@ -3,14 +3,14 @@ class NoSuchStrategyError < StandardError ; end
 
 def rps_game_winner(game)
 	raise WrongNumberOfPlayersError unless game.length == 2 
-	game[0][1].upcase!
-	game[1][1].upcase!
+	#game[0][1].upcase!
+	#game[1][1].upcase!
 	str = ['R', 'P', 'S']
 	if(!str.member?(game[0][1]) || !str.member?(game[1][1]))
 		raise NoSuchStrategyError
 	end
 	if(game[0][1] == game[1][1])
-		return game[0]
+		return game[0].inspect
 	elsif((game[0][1] == 'R' && game[1][1] == 'S') || 
 		(game[0][1] == 'S' && game[1][1] == 'P') ||
 		(game[0][1] == 'P' && game[1][1] == 'R'))
@@ -21,24 +21,14 @@ def rps_game_winner(game)
 end
 
 def rps_tournament_winner(tournament)
-	if(tournament[0][0].is_a?(String))
-		tournament = rps_game_winner(tournament)
-	else
-		rps_tournament_winner(tournament[0])
-		rps_tournament_winner(tournament[1])
-	end
+		if(tournament[0][0].class == String)
+			return tournament = rps_game_winner(tournament)
+		else
+			tournament[0] = rps_tournament_winner(tournament[0])
+			tournament[1] = rps_tournament_winner(tournament[1])
+		end
+		if(tournament[0][0].class == String)
+			return tournament = rps_game_winner(tournament)
+		end
 end
-
-puts rps_tournament_winner(
-[
-[
-	[ ["Armando", "P"], ["Dave", "S"] ],
-	[ ["Richard", "R"], ["Michael", "S"] ],
-],
-[
-	[ ["Allen", "S"], ["Omer", "P"] ],
-	[ ["David E.", "R"], ["Richard X.", "P"] ]
-]
-]
-)
 
